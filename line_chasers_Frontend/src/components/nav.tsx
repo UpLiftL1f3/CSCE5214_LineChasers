b4091a1debfbfb948a5f3334ccb92a5f8bce3290
personal <upLiftL1f3@gmail.com>
2024-09-25 22:47:20 -0500

    no frontend

--- line_chasers_Frontend/src/components/nav.tsx
+++ line_chasers_Frontend/src/components/nav.tsx
@@ -1,7 +1,7 @@
 export default function Nav() {
     return (
-        <div className="bg-cyan-400">
-            <div className="flex-center">
+        <div className=" w-full">
+            <div className="flex-center bg-red-200">
                 <h1 className="text-[100px] ">Line Chasers</h1>
             </div>
         </div>
