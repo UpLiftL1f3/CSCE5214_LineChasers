ca99d9ac2592f05f445e3b45729a03065d740914
personal <upLiftL1f3@gmail.com>
2024-09-25 22:48:22 -0500

    remove frontend

--- line_chasers_Frontend/src/components/nav.tsx
+++
@@ -1,9 +0,0 @@
-export default function Nav() {
-    return (
-        <div className=" w-full">
-            <div className="flex-center bg-red-200">
-                <h1 className="text-[100px] ">Line Chasers</h1>
-            </div>
-        </div>
-    );
-}
