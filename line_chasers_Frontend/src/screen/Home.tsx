9df14373a0a972bc6bf5d35b2b7f62d83847c4ae
personal <upLiftL1f3@gmail.com>
2024-09-26 00:03:52 -0500

    frontend partially done

--- line_chasers_Frontend/src/screen/Home.tsx
+++ line_chasers_Frontend/src/screen/Home.tsx
@@ -1,11 +1,13 @@
 import Nav from "../components/nav";
+import ShowRow from "../components/showRow";
 
 export default function Home() {
     return (
         <div className="">
-            <div className="flex column flex-start">
+            <div className="flex column flex-start w-full">
                 <Nav />
             </div>
+            <ShowRow />
         </div>
     );
 }
