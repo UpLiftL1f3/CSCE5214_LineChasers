b0a7364560349d8fad909646571bcf062596df32
personal <upLiftL1f3@gmail.com>
2024-09-25 22:10:01 -0500

    started working on nav

---
+++ line_chasers_Frontend/src/components/nav.tsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+    return (
+        <div className="bg-red-500">
+            <div className="flex-center">
+                <h1>Line Chasers</h1>
+            </div>
+        </div>
+    );
+}
---
+++ line_chasers_Frontend/src/screen/Home.tsx
@@ -0,0 +1,11 @@
+import Nav from "../components/nav";
+
+export default function Home() {
+    return (
+        <div className="">
+            <div className="flex column flex-start">
+                <Nav />
+            </div>
+        </div>
+    );
+}
